4fa7890ca31d7586fd4bacebb9e42ef8e82f4b38
LAngelHDZ <82234456+LAngelHDZ@users.noreply.github.com>
2023-06-30 15:49:22 -0600

    Se modifico para validar la guia scaneada en un manifiestos

--- app/src/main/java/com/example/pliin/apppliin/domain/usecase/GetAllEmployeesUseCase.kt
+++ app/src/main/java/com/example/pliin/apppliin/domain/usecase/GetAllEmployeesUseCase.kt
@@ -1,6 +1,7 @@
 package com.example.pliin.apppliin.domain.usecase
 
 import com.example.pliin.apppliin.data.repositories.EmployeeRepository
+import com.example.pliin.apppliin.domain.model.emproyeeitem.DataEI
 import com.example.pliin.apppliin.domain.model.emproyeeitem.FieldDataEI
 import javax.inject.Inject
 
@@ -8,9 +9,8 @@ import javax.inject.Inject
 class GetAllEmployeesUseCase @Inject constructor(
     private val employeeRepository: EmployeeRepository
 ) {
-
-    /* suspend operator fun invoke():List<FieldDataEI>{
-
-
-     }*/
+     suspend operator fun invoke(): List<DataEI?>? {
+         val response = employeeRepository.getAllEmployeeApi()
+         return  response.response?.data
+     }
 }
