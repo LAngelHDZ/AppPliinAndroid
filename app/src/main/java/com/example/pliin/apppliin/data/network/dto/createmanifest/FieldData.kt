393676c4d99746180526efea9cd4d5521caf46f8
LAngelHDZ <82234456+LAngelHDZ@users.noreply.github.com>
2023-07-10 02:00:13 -0600

    Se corrigio el error que no se creaba el manifiesto si no seleccionabas un operador

--- app/src/main/java/com/example/pliin/apppliin/data/network/dto/createmanifest/FieldData.kt
+++ app/src/main/java/com/example/pliin/apppliin/data/network/dto/createmanifest/FieldData.kt
@@ -4,9 +4,9 @@ data class FieldData(
     val ClavePrincipal: String?,
     val ConsecutivoFolio: String?,
     val Empresa: String?,
-    val NombreOperador: String?,
-    val Ruta: String?,
-    val Totalpqt: String?,
+    val NombreOperador: String? = "",
+    val Ruta: String? = "",
+    val Totalpqt: String? = "",
     val TotaolGuias: String?,
-    val tipoMan: String?
+    val tipoMan: String? = ""
 )
